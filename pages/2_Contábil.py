eb4cce9f889edcfb2fe824a79b2369b5fb98ef39
lourivalpinheirocshub <lourival.cshub@gmail.com>
2025-08-30 03:08:08 -0300

    Update 2_Contábil.py

--- pages/2_Contábil.py
+++ pages/2_Contábil.py
@@ -408,3 +408,50 @@ with tabs[2]:
 
     with table_add_column:
         st.dataframe(st.session_state["nfse"])
+
+footer = """
+    <style>
+    /* Hide default Streamlit footer */
+    footer {visibility: hidden;}
+
+    .footer-custom {
+        position: relative;
+        bottom: 0;
+        width: 100%;
+        text-align: center;
+        font-size: 14px;
+        color: #ffff;
+        padding: 10px 0;
+        margin-top: auto;
+    }
+    </style>
+
+    <div class="footer-custom">
+        © <strong>CSHUB - Caruaru Contábil<strong/> - Todos os direitos reservados
+    </div>
+    """
+st.markdown(footer, unsafe_allow_html=True)
+
+with st.sidebar:
+    footer = """
+    <style>
+    /* Hide default Streamlit footer */
+    footer {visibility: hidden;}
+
+    .footer-custom {
+        position: relative;
+        bottom: 0;
+        width: 100%;
+        text-align: center;
+        font-size: 14px;
+        color: #ffff;
+        padding: 10px 0;
+        margin-top: auto;
+    }
+    </style>
+
+    <div class="footer-custom">
+        © <strong>CSHUB - Caruaru Contábil<strong/> - Todos os direitos reservados
+    </div>
+    """
+    st.markdown(footer, unsafe_allow_html=True)
